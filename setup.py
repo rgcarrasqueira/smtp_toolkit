bca3474f4ec20fa9f08834fbf7a8626709cb4bca
Rogério Carrasqueira <rogerio.carrasqueira@gmail.com>
2016-09-01 12:08:46 -0300

    Addin setup.py for use PIP

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+    name = 'smtp_toolkit',
+    packages = ['smtp_toolkit'],
+    scripts = [],
+    version = '0.0.1',
+    description = 'Provide a set of classes that facilitate testing email servers with Python',
+    author = 'James Polera',
+    author_email = 'james@uncryptic.com',
+    maintainer = 'Rogerio Carrasqueira',
+    maintainer_email = 'rogerio.carrasqueira@gmail.com',
+    url = 'https://github.com/rgcarrasqueira/smtp_toolkit',
+    keywords = ['openrelay', 'mail'],
+    install_requires = [],
+)
---
+++ smtp_toolkit/__init__.py
@@ -0,0 +1,1 @@
+from SMTPServer import SMTPServer
